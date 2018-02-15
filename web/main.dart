229aac2a510f1e1ae149e02812e4919e42c8f0bf
abrodeur <alexanderbrodeur@live.ca>
2018-02-14 23:15:22 -0500

    Initial commit

---
+++ web/main.dart
@@ -0,0 +1,15 @@
+import 'package:angular/angular.dart';
+
+import 'package:phoenix/app_component.dart';
+import 'dart:html';
+
+void main() {
+//  bootstrap(AppComponent);
+ window.console.info("test");
+}
+
+void afterload(){
+
+
+}
+
